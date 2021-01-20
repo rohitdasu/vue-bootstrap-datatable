3b5b18ae59afaf95603415311dc0ddd17675bb9a
Rohitdasu23 <dasurohit123@gmail.com>
2021-01-21 01:47:13 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <DataTable />
+    <!-- <Data /> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import DataTable from './components/DataTable.vue'
+// import Data from './components/Data.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    DataTable,
+    // Data
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
